138bf5e49f5cc7227fcad7a051903e6f59a23039
J.Lauenstein <johannes.lauenstein@siemens.com>
2018-09-26 21:19:43 +0200

    Matrix for native/i386/arm

--- ci/build_all.sh
+++ ci/build_all.sh
@@ -3,56 +3,68 @@
 # Copyright (c) Siemens AG, 2014-2018
 
 echo "===== Cobalt/i386 build ====="
-set -x
+set -v
 
-cp ci/conf.i386.xeno .config
-ci/xenomai/scripts/prepare-kernel.sh --arch=x86 --verbose
-git status -v
-ls -la .config include/ ci/*; 
-time make -j `nproc` bzImage modules
-pushd ci/xenomai; ./scripts/bootstrap; popd
-mkdir xenobuild
-pushd xenobuild
-../ci/xenomai/configure --with-core=cobalt --enable-smp --enable-pshared --host=i686-linux CFLAGS="-m32 -O2" LDFLAGS="-m32"
-make -s -j `nproc` all
-ls -la . xenobuild
-make clean
-popd
-make clean
 
-echo "===== Ipipe/arm build ====="
+if [ "$TARGET" == "i386" ]; then
+    sudo apt-get install -qq gcc-multilib
 
-cp ci/conf.arm.ipipe .config
-grep CONFIG_IPIPE .config
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bzImage modules
-ls -l .config vmlinux
-make clean
+    cp ci/conf.i386.xeno .config
+    ci/xenomai/scripts/prepare-kernel.sh --arch=x86 --verbose
+    git status -v
+    ls -la .config include/ ci/*; 
+    time make -j `nproc` bzImage modules
+    pushd ci/xenomai; ./scripts/bootstrap; popd
+    mkdir xenobuild
+    pushd xenobuild
+    ../ci/xenomai/configure --with-core=cobalt --enable-smp --enable-pshared --host=i686-linux CFLAGS="-m32 -O2" LDFLAGS="-m32"
+    make -s -j `nproc` all
+    ls -la .
+    make -s clean
+    popd
+    make clean
 
-echo "===== NoIpipe/x86 build ====="
+else if [ "$TARGET" == "arm" ]; then
+    sudo apt-get install -qq gcc-arm-linux-gnueabihf
 
-cp ci/conf.x86.noipipe .config
-grep CONFIG_IPIPE .config
-make -j `nproc` bzImage modules
-ls -l .config vmlinux
-make clean
+    echo "===== Ipipe/arm build ====="
 
-echo "===== Ipipe/x86 build ====="
-cp ci/conf.x86.ipipe .config
-grep CONFIG_IPIPE .config
-make -j `nproc` bzImage modules
-ls -l .config vmlinux
-make clean
+    cp ci/conf.arm.ipipe .config
+    grep CONFIG_IPIPE .config
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bzImage modules
+    ls -l .config vmlinux
+    make clean
 
-echo "===== Cobalt/x86 build ====="
-cp ci/conf.x86.xeno .config
-ci/xenomai/scripts/prepare-kernel.sh --arch=x86 --verbose
-git status -v
-ls -la .config include/ ci/*; 
-make -j `nproc` bzImage modules
-pushd ci/xenomai; ./scripts/bootstrap; popd
-# mkdir xenobuild
-pushd xenobuild
-../ci/xenomai/configure --with-core=cobalt --enable-smp --enable-pshared
-make -s -j `nproc` all
-popd
-ls -la . xenobuild
+else if [ "$TARGET" == "native" ]; then
+
+
+    echo "===== NoIpipe/x86 build ====="
+
+    cp ci/conf.x86.noipipe .config
+    grep CONFIG_IPIPE .config
+    make -j `nproc` bzImage modules
+    ls -l .config vmlinux
+    make clean
+
+    echo "===== Ipipe/x86 build ====="
+    cp ci/conf.x86.ipipe .config
+    grep CONFIG_IPIPE .config
+    make -j `nproc` bzImage modules
+    ls -l .config vmlinux
+    make clean
+
+    echo "===== Cobalt/x86 build ====="
+    cp ci/conf.x86.xeno .config
+    ci/xenomai/scripts/prepare-kernel.sh --arch=x86 --verbose
+    git status -v
+    ls -la .config include/ ci/*; 
+    make -j `nproc` bzImage modules
+    pushd ci/xenomai; ./scripts/bootstrap; popd
+    mkdir xenobuild
+    pushd xenobuild
+    ../ci/xenomai/configure --with-core=cobalt --enable-smp --enable-pshared
+    make -s -j `nproc` all
+    popd
+    ls -la . xenobuild
+
+fi
