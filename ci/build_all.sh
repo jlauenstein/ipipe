bdd3b6458483c09eeca3942a64b6b6f49368bb19
J.Lauenstein <johannes.lauenstein@siemens.com>
2018-10-15 20:24:19 +0200

    add conf.i386.noipipe

--- ci/build_all.sh
+++ ci/build_all.sh
@@ -16,10 +16,8 @@ build_xeno()
     
     # use all supplied extra args for configure
     ../ci/xenomai/configure --with-core=cobalt --enable-smp  "$@"
-#    cat config.log
 
     make -s -j `nproc` all
-    file utils/ps/rtps
     popd
 }
 
@@ -28,10 +26,17 @@ if [ "$TARGET" == "i386" ]; then
 
     sudo apt-get install -qq gcc-multilib
 
+    echo "===== NoIpipe/i386 build ====="
+
+    cp ci/conf.i386.noipipe .config
+    make ARCH=i386 -j `nproc` bzImage modules
+    ls -l .config vmlinux
+    make -s clean
+
     echo "===== Ipipe/i386 build ====="
 
     cp ci/conf.i386.ipipe .config
-    time make -j `nproc` bzImage modules
+    make ARCH=i386 -j `nproc` bzImage modules
     ls -l .config vmlinux
     make -s clean
 
@@ -39,7 +44,7 @@ if [ "$TARGET" == "i386" ]; then
 
     cp ci/conf.i386.xeno .config
     ci/xenomai/scripts/prepare-kernel.sh --arch=x86 --verbose
-    time make -j `nproc` bzImage modules
+    make ARCH=i386 -j `nproc` bzImage modules
     ls -l .config vmlinux
 
     build_xeno --enable-pshared --host=i686-linux "CFLAGS=-m32 -O2" "LDFLAGS=-m32"
@@ -48,19 +53,18 @@ if [ "$TARGET" == "i386" ]; then
 elif [ "$TARGET" == "arm" ]; then
 
     sudo apt-get install -qq gcc-arm-linux-gnueabihf
-#    sudo apt-get install -y pkg-config-arm-linux-gnueabihf
 
    echo "===== NoIpipe/arm build ====="
 
     cp ci/conf.arm.noipipe .config
-    time make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bzImage modules
+    make ARCH=arm -j `nproc` CROSS_COMPILE=arm-linux-gnueabihf- bzImage modules
     ls -l .config vmlinux
     make clean
 
     echo "===== Ipipe/arm build ====="
 
     cp ci/conf.arm.ipipe .config
-    time make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bzImage modules
+    make ARCH=arm -j `nproc` CROSS_COMPILE=arm-linux-gnueabihf- bzImage modules
     ls -l .config vmlinux
     make clean
 
@@ -68,7 +72,7 @@ elif [ "$TARGET" == "arm" ]; then
 
     cp ci/conf.arm.xeno .config
     ci/xenomai/scripts/prepare-kernel.sh --arch=arm --verbose
-    time make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bzImage modules
+    make ARCH=arm -j `nproc` CROSS_COMPILE=arm-linux-gnueabihf- bzImage modules
     ls -l .config vmlinux
 
     build_xeno --build=i686-pc-linux-gnu --host=arm-linux-gnueabihf CC=arm-linux-gnueabihf-gcc "CFLAGS=-march=armv7-a -mfpu=vfp3" "LDFLAGS=-march=armv7-a -mfpu=vfp3"
